0c994a38624b4f43a74fe6198cee9e8c25652f08
edupezzijoli <eduardo.pezzi@vinhosjolimont.com.br>
2025-03-09 19:11:41 -0300

    update

--- examples/simple.py
+++ examples/simple.py
@@ -12,8 +12,12 @@ llm = ChatOpenAI(
 	model='gpt-4o-mini',
 	temperature=0.0,
 )
-task = 'Encontre informações atuais dos meus concorrentes Arco, Songz, VMG Aires, e Sanz Clima, quero saber dos lançamentos e inovações'
+task = """
+acesse o site https://www.salton.com.br/blog encontre todos os titulos dos artigos do blog, rolando para baixo para carregar, tem que esperar ele carregar novos artigos, só pare quando encotrar todos.
+depois faça um resumo com os principais temas e as 10 palavras mais frequentes
+por fim sugira novos temas que ainda não foram falados mas são de interesse dos apreciadores de vinho em português seguindo as tendências e as dicas dos especialistas. 
 
+"""
 agent = Agent(task=task, llm=llm)
 
 
@@ -23,3 +27,21 @@ async def main():
 
 if __name__ == '__main__':
 	asyncio.run(main())
+
+
+
+
+
+# """
+# pesquise na web sites que vendam esses produtos e encontre os melhores preços. 
+# Ao final crie uma tabela com o nome do item o link do site com menor valor e o valor. e some o valor total dos itens.
+
+# Fonte - Cooler Master MWE 750W, 80 Plus Bronze
+# Placa de Vídeo - MSI RTX 3050 Ventus 2X XS OC NVIDIA GeForce, 8GB GDDR6
+# Processador - AMD Ryzen 7 5700, 8-Core, 16-Threads, 3.7GHz (4.6GHz Turbo), Cache 20MB
+# Cooler para Processador - DeepCool High Performance AK400, 120mm, Intel-AMD
+# Gabinete Gamer - Rise Mode Glass 06X, Mid Tower, ATX, Lateral em Vidro Fumê e Frontal em Vidro Temperado
+# Memória RAM - Kingston Fury Beast, 16GB (2x8GB), 3600MHz, DDR4, CL17
+# SSD - Kingston NV2, 1TB, M.2 NVMe, 2280, Leitura 3500MB/s e Gravação 2100MB/s
+# Placa-Mãe - MSI B550M PRO-VDH, Chipset B550, AMD AM4, mATX, DDR4
+# """
